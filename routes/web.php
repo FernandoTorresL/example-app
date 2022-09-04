bb07c3014c18a32a966dcca549f79583e2e35dcf
Fer Torres <fertorresmx@gmail.com>
2022-09-03 20:54:00 -0500

    Add some routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // Consulta a base de datos
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    // Consulta a base de datos
+    return $request->all();
 });
