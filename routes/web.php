c2eae443624a817c779bf08bd0c63ad09909cbd7
Fer Torres <fertorresmx@gmail.com>
2022-09-04 00:28:37 -0500

    Add code from class 'Eloquent'

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('blog/{slug}',   [PageController::class,   'post'])->name('post');
 
 // Para no repetir el controlador, se incluyen en un grupo
 Route::controller(PageController::class)->group(function () {
-    Route::get('/',             'home')->name('home');
-    Route::get('blog',          'blog')->name('blog');
-    Route::get('blog/{slug}',   'post')->name('post');
+    Route::get('/',                 'home')->name('home');
+    Route::get('blog',              'blog')->name('blog');
+    Route::get('blog/{post:slug}',  'post')->name('post');
 });
