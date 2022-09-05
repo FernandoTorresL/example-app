0e77f613153d7bf9b7fcaa2b2522f41e67e3dea9
Fer Torres <fertorresmx@gmail.com>
2022-09-04 23:08:38 -0500

    Install Breeze

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-Route::get('/',             [PageController::class,   'home'])->name('home');
-Route::get('blog',          [PageController::class,   'blog'])->name('blog');
-Route::get('blog/{slug}',   [PageController::class,   'post'])->name('post');
-*/
-
 // Para no repetir el controlador, se incluyen en un grupo
 Route::controller(PageController::class)->group(function () {
     Route::get('/',                 'home')->name('home');
     Route::get('blog',              'blog')->name('blog');
     Route::get('blog/{post:slug}',  'post')->name('post');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
